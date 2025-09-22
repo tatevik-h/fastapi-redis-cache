f731e9e1d00b185489cf97ca790c3528802567a6
tatevik-h <tatevikharutyunyan99.99@gmail.com>
2025-09-22 21:02:03 +0400

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
-import pytest
+import pytest_asyncio
 from httpx import AsyncClient
 from src.main import app
 
 
-@pytest.fixture()
+@pytest_asyncio.fixture
 async def client():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
