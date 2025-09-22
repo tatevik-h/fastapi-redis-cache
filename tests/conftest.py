0796f4991c874eb927b6155b9a1fd95efe6494c3
tatevik-h <tatevikharutyunyan99.99@gmail.com>
2025-09-22 22:56:14 +0400

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ from httpx import AsyncClient
 from unittest.mock import AsyncMock, patch
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlalchemy.orm import sessionmaker
 from src.main import app
 from src.db.db import get_db
+
 
 @pytest.fixture(autouse=True)
 def mock_cache():
@@ -20,16 +21,19 @@ def mock_cache():
 DATABASE_URL = "sqlite+aiosqlite:///:memory:"
 
 @pytest_asyncio.fixture
-async def session():
-    engine = create_async_engine(DATABASE_URL, echo=False)
+async def session() -> AsyncSession:
+    engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=False)
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
-    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async_session = sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
     async with async_session() as s:
         yield s
 
 @pytest_asyncio.fixture
 async def client(session: AsyncSession):
+    # Override FastAPI dependency
     app.dependency_overrides[get_db] = lambda: session
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
